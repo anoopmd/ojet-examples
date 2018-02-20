307ed8879684e91c6f0c654c097a1263ec8b7424
Anoop M D <anoop.md@sinilabs.co.in>
2018-02-21 00:19:29 +0530

    Ojet Table Example

--- src/js/models/user.js
+++ src/js/models/user.js
@@ -1,7 +1,15 @@
 define(['ojs/ojcore', 'ojs/ojmodel'], function(oj){
   UserModel =  oj.Model.extend({
     urlRoot: 'http://localhost:5000/api/users',
-    idAttribute:'id'
+    idAttribute:'id',
+    parse: function(data) {
+      console.log(data);
+
+      return {
+        id: data.id,
+        name: 'John Nash'
+      };
+    }
   });
   
   return UserModel;
--- src/main.js
+++ src/main.js
@@ -1,22 +1,20 @@
-<script type="text/javascript" src="../node_modules/requirejs/require.js"></script>
-
-<script type="text/javascript">
 requirejs.config({
   baseUrl: '.',
-
   paths:  {
     'knockout': '../node_modules/knockout/build/output/knockout-latest',
     'jquery': '../node_modules/jquery/dist/jquery.min',
     'promise': '../node_modules/es6-promise/dist/es6-promise',
     'hammerjs': '../node_modules/hammerjs/hammer',
     'ojdnd': '../node_modules/@oracle/oraclejet/dist/js/libs/dnd-polyfill/dnd-polyfill-1.0.0',
-    'ojs': '../node_modules/@oracle/oraclejet/dist/js/libs/oj/min',
+    'ojs': '../node_modules/@oracle/oraclejet/dist/js/libs/oj/debug',
     'ojL10n': '../node_modules/@oracle/oraclejet/dist/js/libs/oj/ojL10n',
     'ojtranslations': '../node_modules/@oracle/oraclejet/dist/js/libs/oj/resources',
     'text': '../node_modules/requirejs-text/text',
     'signals': '../node_modules/signals/dist/signals',
     'proj4': '../node_modules/proj4js/dist/proj4-src',
     'css': '../node_modules/require-css/css',
+    'customElements': '../node_modules/@webcomponents/custom-elements/custom-elements.min',
+    'jqueryui-amd': '../node_modules/jquery-ui/ui'
   },
   shim: {
     'jquery': {
@@ -24,8 +22,3 @@ requirejs.config({
     }
   }
 });
-
-require(['ojs/ojcore', 'knockout'], function(oj, ko, app) {
-  console.log('Hello World');
-});
-</script>
